b44b49d36e551caaed04c4f73bfc4926e4cf9c6d
Konstantin Chkhaidze <lynx.ua@gmail.com>
2013-06-18 11:18:36 +0300

    Upd: restore facebook_js template tag

--- socialregistration/contrib/facebook/templatetags/facebook.py
+++ socialregistration/contrib/facebook/templatetags/facebook.py
@@ -1,7 +1,12 @@
 from django import template
-
+from django.conf import settings
 from socialregistration.templatetags import button
+from socialregistration.utils import is_https
 
 register = template.Library()
 
 register.tag('facebook_button', button('socialregistration/facebook/facebook_button.html'))
+
+@register.inclusion_tag('socialregistration/facebook_js.html')
+def facebook_js():
+    return {'facebook_api_key': settings.FACEBOOK_API_KEY, 'is_https': is_https()}
--- socialregistration/utils.py
+++ socialregistration/utils.py
@@ -1,7 +1,19 @@
 import uuid
+from django.conf import settings
+
+USE_HTTPS = bool(getattr(settings, 'SOCIALREGISTRATION_USE_HTTPS', False))
+
 
 def generate_username(user, profile, client):
     """
     Default function to generate usernames using the built in `uuid` library.
     """
     return str(uuid.uuid4())[:30]
+
+
+def is_https():
+    """
+    Check if the site is using HTTPS. This is controlled with
+    ``SOCIALREGISTRATION_USE_HTTPS`` setting.
+    """
+    return USE_HTTPS
